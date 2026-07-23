e6cda23c04d7cbbb5697d5baf8a0e3bcc025d5d5
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-23 12:18:13 -0500

    Refactor main function and loop structure

---
+++ mod_3_exam(ShoppingCart).cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "ShoppingCart.h"
+
+using namespace std;
+
+int main(){
+    ShoppingCart cart(5);
+
+    ShoppingCart::setSalesTax(0.095);
+
+    Item item1("Milk",3.49);
+    Item item2("Bread",2.99);
+    Item item3("Eggs",4.25);
+
+    cart = cart + item1;
+    cart = cart + item2;
+    cart = cart + item3;
+
+    cout << "Shopping Cart" << endl;
+    cout << "-------------" << endl;
+
+    for(int i=0;i<cart.getItemCount();i++){
+        cout << cart[i].getName()
+             << " $"
+             << cart[i].getPrice()
+             << endl;
+    }
+
+    ShoppingCart cart2(cart);
+
+    cout << endl;
+    cout << "Copied Cart" << endl;
+
+    for(int i=0;i<cart2.getItemCount();i++){
+        cout << cart2[i].getName()
+             << " $"
+             << cart2[i].getPrice()
+             << endl;
+    }
+
+    ShoppingCart cart3;
+    cart3 = cart;
+
+    cout << endl;
+    cout << "Assigned Cart" << endl;
+
+    for(int i=0;i<cart3.getItemCount();i++){
+        cout << cart3[i].getName()
+             << " $"
+             << cart3[i].getPrice()
+             << endl;
+    }
+}
